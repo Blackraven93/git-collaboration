ae34ec1acac8c80404b18182d49db71d5a3dc864
ttltna <ttltnaaa@gmail.com>
2021-12-05 20:42:22 +0900

    feat : 더하기 빼기 곱하기 나누기를 화살표표기법 매소드로 구현하기

---
+++ calculate.js
@@ -0,0 +1,9 @@
+let Add = (a, b) => { return a + b; };
+let Subtract = (a ,b) => { return a - b; };
+let Multiplay = (a, b) => { return a * b; };
+let Divide = (a, b) => { return a / b; };
+
+console.log(Add(100,5));
+console.log(Subtract(230,86));
+console.log(Multiplay(15,19));
+console.log(Divide(10,2));
