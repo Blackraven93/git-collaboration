42b7b608026cd00cb28a4447667ed2dc9d2409b1
clwogns <clwogns@naver.com>
2021-12-05 22:07:43 +0900

    Feat : change variable name and give new typle

--- 최재훈/calculate.js
+++ 최재훈/calculate.js
@@ -1,20 +1,32 @@
 
 
-let plus =(a,b)=>{
-     return`${a}+${b}=${a+b}`
+let Add =(a,b)=>{
+     return  `${a}+${b}=${parseInt(a+b)}`;
+ 
 };
 
 
-let minus =(a,b)=>{
-    return`${a}-${b}=${a-b}` 
+let Subtract = (a,b)=>{
+    return `${a}-${b}=${parseInt(a-b)}`;
+   
  };
  
 
- let multiplication =(a,b)=>{
-    return`${a}*${b}=${a*b}`
+ let Multiply =(a,b)=>{
+    return `${a}*${b}=${parseInt(a*b)}`;
+  
  };
  
- let division =(a,b)=>{
-    return`${a}/${b}=${a/b}`
+ let Divide = (a,b)=>{
+    return `${a}/${b}=${parseInt(a/b)}`;
+  
+
  };
- 
+const test =  Add(10,5);
+const test2 = Subtract(10,5);
+const test3 = Multiply(10,5);
+const test4 = Divide(10,5);
+console.log(test);
+console.log(test2);
+console.log(test3);
+console.log(test4);
