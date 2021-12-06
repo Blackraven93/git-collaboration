7c0e71037ce179742ba76718f8e943e65c3c6c76
clwogns <clwogns@naver.com>
2021-12-06 09:27:14 +0900

    Featv: change name

--- 최재훈/calculate.js
+++ 최재훈/calculate.js
@@ -1,31 +1,31 @@
 
 
-let Add =(a,b)=>{
+let add =(a,b)=>{
      return  `${a}+${b}=${parseInt(a+b)}`;
  
 };
 
 
-let Subtract = (a,b)=>{
+let subtract = (a,b)=>{
     return `${a}-${b}=${parseInt(a-b)}`;
    
  };
  
 
- let Multiply =(a,b)=>{
+ let multiply =(a,b)=>{
     return `${a}*${b}=${parseInt(a*b)}`;
   
  };
  
- let Divide = (a,b)=>{
+ let divide = (a,b)=>{
     return `${a}/${b}=${parseInt(a/b)}`;
   
 
  };
-const test =  Add(10,5);
-const test2 = Subtract(10,5);
-const test3 = Multiply(10,5);
-const test4 = Divide(10,5);
+const test =  add(10,5);
+const test2 = subtract(10,5);
+const test3 = multiply(10,5);
+const test4 = divide(10,5);
 console.log(test);
 console.log(test2);
 console.log(test3);
